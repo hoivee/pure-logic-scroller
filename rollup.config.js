e3265ad433874ec909857cebf90fa30805a451b2
hoivee <xjhovee@qq.com>
2019-12-02 23:05:27 +0800

    add dist banner

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,29 @@
 import path from 'path'
 import typescript from 'rollup-plugin-typescript2'
 import { eslint } from "rollup-plugin-eslint"
-const version = process.env.VERSION || require('./package.json').version
+const version = require('./package.json').version
+const banner = ''+
+'/*!\n' +
+` * pure-logic-scroller.js v${version}\n` +
+` * (c) 2019-${new Date().getFullYear()} hoivee\n` +
+' * Released under the MIT License.\n' +
+' */'
 export default {
   input: path.resolve(__dirname, './src/index.ts'),
   output: [
     {
       file: path.resolve(__dirname, './dist/index.es.js'),
       format: 'es',
+      banner,
       name: 'scroller',
     },
     {
       file: path.resolve(__dirname, './dist/index.js'),
+      banner,
       format: 'umd',
       name: 'scroller',
     },
   ],
-  banner:''+
-    '/*!\n' +
-    ` * scroller.js v${version}\n` +
-    ` * (c) 2019-${new Date().getFullYear()} hoivee\n` +
-    ' * Released under the MIT License.\n' +
-    ' */',
-  sourcemap:'inline',
   plugins: [
     eslint(),
     typescript({
